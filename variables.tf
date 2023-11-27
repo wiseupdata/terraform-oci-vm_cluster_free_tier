d194c8019db6902fd669c7313396b8890c223cc5
wiseupdata <silvio.liborio@wiseupdata.com>
2023-11-27 17:46:42 +0000

    sync

---
+++ variables.tf
@@ -0,0 +1,47 @@
+########################################
+# Optional 
+########################################
+variable "company_name" {
+  description = "The name of the company."
+  type        = string
+  default     = "wiseupdata"
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+  default     = "dl"
+}
+
+variable "compartment_id" {
+  description = "The OCID of the compartment where the resources will be created. You can find the compartment ID in the OCI Console by navigating to the compartment where you want to create the resources and copying the OCID from the compartment details page."
+  type        = string
+  default     = "auto-create"
+}
+
+variable "env" {
+  description = "System environment."
+  type        = string
+  default     = "develop"
+}
+
+variable "default_tags" {
+  description = "Default tags for all resources"
+  type        = map(string)
+  default = {
+    "auto-create" = "true"
+  }
+}
+
+locals {
+
+  basic_tags = {
+    "managed_by" : "terraform",
+    "app_name" : var.app_name,
+    "env" : var.env,
+    "company" : var.company_name
+  }
+
+  compartment_name = "cp-${var.app_name}-${var.company_name}-${var.env}"
+
+}
---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "~>5.21"
+    }
+  }
+}
+
+provider "oci" {
+}
