fbd17228e1b9d352fde3eb21f352c1ec0f65f107
wiseupdata <silvio.liborio@wiseupdata.com>
2023-11-30 21:21:38 +0000

    sync

--- assets/scripts/one-click-deployment.sh
+++ assets/scripts/one-click-deployment.sh
@@ -1,8 +1,8 @@
 #Deploy on click
-git add . && git commit -am "sync" && git push && git tag 0.0.1 -f && git push --tags -f
+git add . && git commit -am "sync" && git push && git tag 0.0.2 -f && git push --tags -f
 
 
 # Create the branch and tag
-git checkout -b 0.0.1 && git push --set-upstream origin 0.0.1
+git checkout -b 0.0.2 && git push --set-upstream origin 0.0.2
 
 
