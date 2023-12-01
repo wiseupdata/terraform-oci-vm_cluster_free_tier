a71485be3ba54cdc8da5290f4767d88114d587d4
wiseupdata <silvio.liborio@wiseupdata.com>
2023-12-01 17:38:01 +0000

    sync

--- assets/scripts/one-click-deployment.sh
+++ assets/scripts/one-click-deployment.sh
@@ -3,8 +3,8 @@ git add . && git commit -am "sync" && git push && git tag 0.0.1 -f && git push -
 
 
 # Create the branch and tag
-git checkout -b 0.0.1 && git push --set-upstream origin 0.0.1
+git checkout -b 0.0.2 && git push --set-upstream origin 0.0.2
 
 
 # Rename project locally
-git remote set-url origin git@github.com:wiseupdata/terraform-oci-cluster-free-tier.git
+# git remote set-url origin git@github.com:wiseupdata/terraform-oci-cluster-free-tier.git
