29aed13903e74d660f2f1354fb0bdb19acfbb894
wiseupdata <silvio.liborio@wiseupdata.com>
2023-12-01 16:58:40 +0000

    sync

--- datasources.tf
+++ datasources.tf
@@ -2,7 +2,7 @@ data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
 }
 
-data "oci_core_images" "ubuntu_images" {
-  compartment_id = var.compartment_id
-  display_name   = var.image_name
-}
+# data "oci_core_images" "ubuntu_images" {
+#   compartment_id = var.compartment_id
+#   display_name   = var.image_name
+# }
